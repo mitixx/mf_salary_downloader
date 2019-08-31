468b647e7e1c3504694f38d63f576e9a2b6eb074
未知 <haruokame05@yahoo.co.jp>
2019-08-31 17:35:52 +0900

    add:init

--- downloader.js
+++ downloader.js
@@ -1,1 +1,22 @@
 const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({headless:false});
+    const page = await browser.newPage();
+    const ap = await page
+    ap.goto('https://payroll.moneyforward.com/',{waitUntil:'domcontentloaded'});
+    const user = {
+        'email':'',
+        'pass':''
+    }
+    let month = 8
+    ap.type('#sign_in_session_service_email',user.email);
+    ap.type('#sign_in_session_service_password',user.pass);
+    page.goto('https://payroll.moneyforward.com/my/reports/payslips',{waitUntil:'domcontentloaded'});
+    page.click('div.period-list');
+    for (let l = 0; l < month; l++) {
+        ap.select('select.dropdown-menu')
+        page.click('a.pa-btn-setting pa-btn-size-xxsmall');
+    }
+    await browser.close();
+})
